8e97fef1439e1c19fe48fe8a9da77351319167b1
Parth Kheni <kheni.parth@gmail.com>
2017-07-28 17:55:40 -0400

    make call to strongview web api with alamofire

--- TechMuzzJsonParser/ViewController.swift
+++ TechMuzzJsonParser/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    var searchUrl = "https://api.spotify.com/v1/search?q=Imagine+Dragons&type=track"
+    typealias JSONStandard = [String: AnyObject]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchUrl)
+    }
+    
+    func callAlamo(url: String) {
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData: Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            print(readableJSON)
+        }
+        catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
