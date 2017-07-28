ccd233d43c54bfd4b3865621c4853cbde25911fa
Parth Kheni <kheni.parth@gmail.com>
2017-07-28 18:25:03 -0400

    scrap data from api

--- TechMuzzJsonParser/ViewController.swift
+++ TechMuzzJsonParser/ViewController.swift
@@ -11,7 +11,13 @@ import Alamofire
 
 class TableViewController: UITableViewController {
     
+    var names = [String]()
     var searchUrl = "https://api.spotify.com/v1/search?q=Imagine+Dragons&type=track"
+    let headers = [
+        "Authorization": "ADD-O-AUTH-TOKEN-HERE",
+        "Accept": "application/json"
+    ]
+
     typealias JSONStandard = [String: AnyObject]
 
     override func viewDidLoad() {
@@ -21,7 +27,7 @@ class TableViewController: UITableViewController {
     }
     
     func callAlamo(url: String) {
-        Alamofire.request(url).responseJSON(completionHandler: {
+        Alamofire.request(url, headers: headers).responseJSON(completionHandler: {
             response in
             self.parseData(JSONData: response.data!)
         })
@@ -29,12 +35,33 @@ class TableViewController: UITableViewController {
     
     func parseData(JSONData: Data) {
         do {
-            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
-            print(readableJSON)
+            let readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard {
+                if let items = tracks["items"] {
+                    for i in 0..<items.count {
+                        let item = items[i] as! JSONStandard
+                        
+                        let name = item["name"] as! String
+                        names.append(name)
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
         }
         catch {
             print(error)
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell")
+        cell?.textLabel?.text = names[indexPath.row]
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
